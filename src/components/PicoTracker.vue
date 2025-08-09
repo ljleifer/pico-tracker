0c1b6f737ff38d66b80cec07d293fa989962773e
Logan <logan.leifer@gmail.com>
2025-08-08 23:31:27 -0500

    session 1 upgrades

--- src/components/PicoTracker.vue
+++ src/components/PicoTracker.vue
@@ -53,6 +53,10 @@
         <input type="text" v-model="character.characterName">
         <p>Player Name:</p>
         <input type="text" v-model="character.playerName">
+        <p>Negative Mood</p>
+        <input type="text" v-model="character.negativeMood">
+        <p>Positive Mood</p>
+        <input type="text" v-model="character.positiveMood">
         <p>Aspects:</p>
         <div v-if="character.aspects.length == 0" class="box">
           <p>NONE</p>
@@ -74,11 +78,30 @@
         <p>No Characters</p>
       </div>
       <div v-else v-for="character in characterData" class="box column fixed">
-        <p>{{ character.characterName }}</p>
-        <p>{{ character.playerName }}</p>
-        <p>Aspects:</p>
+        <div class="mood">
+          <div style="flex: 1;"></div>
+          <p style="margin-right: 3px;">{{ character.negativeMood }}</p>
+          <img v-if="character.mood == '1'" src="../assets/circle-x.png"/>
+          <img v-else src="../assets/circle.png" @click="setMood(character, '1')"/>
+          <img src="../assets/dash.png" style="cursor: default;"/>
+          <img v-if="character.mood == '2'" src="../assets/circle-x.png"/>
+          <img v-else src="../assets/circle.png" @click="setMood(character, '2')"/>
+          <img src="../assets/dash.png" style="cursor: default;"/>
+          <img v-if="character.mood == '3'" src="../assets/circle-x.png"/>
+          <img v-else src="../assets/circle.png" @click="setMood(character, '3')"/>
+          <img src="../assets/dash.png" style="cursor: default;"/>
+          <img v-if="character.mood == '4'" src="../assets/circle-x.png"/>
+          <img v-else src="../assets/circle.png" @click="setMood(character, '4')"/>
+          <img src="../assets/dash.png" style="cursor: default;"/>
+          <img v-if="character.mood == '5'" src="../assets/circle-x.png"/>
+          <img v-else src="../assets/circle.png" @click="setMood(character, '5')"/>
+          <p style="margin-left: 3px;">{{ character.positiveMood }}</p>
+          <div style="flex: 1;"></div>
+        </div>
+        <p class="center">{{ character.characterName }}</p>
+        <p class="center">{{ character.playerName }}</p>
         <div v-if="character.aspects.length == 0" class="box">
-          <p>NONE</p>
+          <p>No Aspects</p>
         </div>
         <div v-else v-for="aspect, aspectIndex in character.aspects" class="box column" style="margin: 5px;">
           <p>{{ aspect.name }}</p>
@@ -112,6 +135,9 @@ export default {
       this.characterData.push({
         characterName: '',
         playerName: '',
+        mood: '3',
+        negativeMood: '',
+        positiveMood: '',
         aspects: [],
       })
     },
@@ -136,6 +162,9 @@ export default {
     },
     deleteAspect(characterIndex, aspectIndex) {
       this.characterData[characterIndex].aspects.splice(aspectIndex, 1)
+    },
+    setMood(character, mood) {
+      character.mood = mood
     },
     addNewTrack() {
       this.trackData.push({
@@ -162,8 +191,8 @@ export default {
     },
   },
   async beforeMount() {
-    this.characterData = JSON.parse(localStorage.getItem('picoCharacterData')) || [{"characterName":"Guinness the Beetle","playerName":"Logan","aspects":[{"name":"Widewings","health":"hhh"},{"name":"Tankling + Rotund","health":"h.h.hh"},{"name":"Angulomorph","health":"h."},{"name":"Blockomorph","health":"h."}]},{"characterName":"Inwë Ælensar","playerName":"Kristian","aspects":[{"name":"Widewings + Hydrophobic","health":"hhh.hh"},{"name":"Thimblefriend + Exaggero","health":"hhhh"},{"name":"Sewing Needle","health":"hhhh"}]},{"characterName":"Xana Goodfellow","playerName":"Ethan","aspects":[{"name":"Hardshell + Ornate","health":"h.hhh.hh"},{"name":"Mind Palace","health":"hh"},{"name":"Logic Center","health":"hhhh"},{"name":"Eurekaling","health":"hhh"}]},{"characterName":"Shadowsting the Dracomantis","playerName":"Nick","aspects":[{"name":"Mandibles + Cannibalizing + Xenic","health":"hh.hhh.h"},{"name":"Many-Eyes","health":"h.h"},{"name":"Flutterflaps","health":"hh"},{"name":"Goggles","health":"hhhhh"}]}]
-    this.trackData = JSON.parse(localStorage.getItem('picoTrackData')) || [{"name":"test","progress":"..|.."},{"name":"also","progress":"..."}]
+    this.characterData = JSON.parse(localStorage.getItem('picoCharacterData')) || [{"characterName":"Guinness the Beetle","playerName":"Logan","aspects":[{"name":"Widewings","health":"hhh"},{"name":"Tankling + Rotund","health":"h.h.hh"},{"name":"Angulomorph","health":"h."},{"name":"Blockomorph","health":"h."}],"negativeMood":"Scornful","positiveMood":"Content","mood":"3"},{"characterName":"Inwë Ælensar","playerName":"Kristian","aspects":[{"name":"Widewings + Hydrophobic","health":"hhh.hh"},{"name":"Thimblefriend + Exaggero","health":"hhhh"},{"name":"Sewing Needle","health":"hhhh"}],"negativeMood":"Scornful","positiveMood":"Focused","mood":"3"},{"characterName":"Xana Goodfellow","playerName":"Ethan","aspects":[{"name":"Hardshell + Ornate","health":"h.hhh.hh"},{"name":"Mind Palace","health":"hh"},{"name":"Logic Center","health":"hhhh"},{"name":"Eurekaling","health":"hhh"}],"positiveMood":"Creative","negativeMood":"Detached","mood":"3"},{"characterName":"Shadowsting the Dracomantis","playerName":"Nick","aspects":[{"name":"Mandibles + Cannibalizing + Xenic","health":"hh.hhh.h"},{"name":"Many-Eyes","health":"h.h"},{"name":"Flutterflaps","health":"hh"},{"name":"Goggles","health":"hhhhh"}],"negativeMood":"Snappy","positiveMood":"Jaunty","mood":"3"}]
+    this.trackData = JSON.parse(localStorage.getItem('picoTrackData')) || []
   },
 }
 </script>
@@ -193,10 +222,16 @@ export default {
   padding: 10px;
 }
 .fixed {
-  min-width: 300px;
+  min-width: 350px;
 }
 .column {
   flex-direction: column;
+}
+.mood > img {
+  margin: 2px 0;
+}
+.center {
+  margin: 0 auto;
 }
 img {
   height: 20px;
